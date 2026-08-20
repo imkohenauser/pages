d97411cc722432850ca832c1dffde31b8543266b
Kohei Saito <iehokotias@me.com>
2026-08-20 16:11:21 +0900

    refactor: ヘッダーの共通レイアウト集約とトップページのセクション構成

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,12 +1,12 @@
 export const site = {
-  name: 'Name',
-  bio: 'Short bio.',
-  avatar: '',
+  name: 'Kohei Saito, or 廣円 (Kohen)',
+  title: 'Personal site of a Design Engineer, or Creative Technologist',
+  bio: 'Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus leo eu aenean sed diam urna tempor.',
 };
 
 export const projects = [
   {
-    name: 'Project',
+    name: 'Sample Project 1',
     text: 'Short project description.',
     url: 'https://imkohenauser.com/',
   },
